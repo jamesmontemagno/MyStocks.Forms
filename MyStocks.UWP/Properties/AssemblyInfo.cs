8f9d0db71e082b3c7b9f7ed109ae42e2f4f1a338
Jason Short <jshort@microsoft.com>
2016-10-05 13:01:53 -0700

    Added debug break if we get an exception

--- MyStocks.UWP/Properties/AssemblyInfo.cs
+++ MyStocks.UWP/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyStocks.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MyStocks.Forms.UWP")]
+[assembly: AssemblyDescription("MyStocks.Forms.UWP")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyStocks.UWP")]
+[assembly: AssemblyProduct("MyStocks.Forms.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
+
